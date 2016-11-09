28b713bed5424f3b5a9876ea26c3bcc230090f93
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-11-09 09:54:48 +0200

    task 7 solved

--- week7-week7_07.Dictionary/src/TextUserInterface.java
+++ week7-week7_07.Dictionary/src/TextUserInterface.java
@@ -13,14 +13,40 @@ public class TextUserInterface {
 
     public void start() {
         System.out.println("Statement:\n"
-                + "  quit - quit the text user interface");
-        System.out.println("Statement: ");
-        String command = reader.nextLine();
-        if (command.equals("quit")) {
-            System.out.println("Cheers!");
-        } else {
-            System.out.println("Unknown statement");
+                + " quit - quit the text user interface\n"
+                + " add - adds a word pair to the dictionary\n"
+                + " translate - asks a word and prints its translation\n");
+
+        while (true) {
+            System.out.println("Statement: ");
+            String command = reader.nextLine();
+            if (command.equals("quit")) {
+                System.out.println("Cheers!");
+                break;
+            } else if (command.equals("add")) {
+                this.add();
+            } else if (command.equals("translate")) {
+                this.translate();
+            } else {
+                System.out.println("Unknown statement");
+            }
         }
+
+    }
+
+    public void add() {
+        System.out.println("In Finnish: ");
+        String finnishWord = reader.nextLine();
+        System.out.println("Translation: ");
+        String englishWord = reader.nextLine();
+        dictionary.add(finnishWord, englishWord);
+    }
+
+    public void translate() {
+        System.out.println("Give a word: ");
+        String word = reader.nextLine();
+        String translatedWord = this.dictionary.translate(word);
+        System.out.println("Translation: " + translatedWord);
         
     }
 
