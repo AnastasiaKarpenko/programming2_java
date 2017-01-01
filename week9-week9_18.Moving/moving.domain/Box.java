777d7c680a27874237ea2800676d32b4b361163a
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-01 23:07:22 +0300

    18.3

---
+++ week9-week9_18.Moving/moving.domain/Box.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anastasia on 01/01/2017.
+ */
+public class Box implements Thing {
+    private int maximumCapacity;
+    private List<Thing> things;
+
+    public Box(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+        things = new ArrayList<>();
+    }
+
+    public boolean addThing(Thing thing) {
+        if (thing.getVolume() > (this.maximumCapacity - this.getVolume())) {
+            return false;
+        } else {
+            things.add(thing);
+            return true;
+        }
+
+    }
+
+    @Override
+    public int getVolume() {
+        int volume = 0;
+        for (Thing thing : things) {
+            volume += thing.getVolume();
+        }
+
+        return volume;
+    }
+
+}
