ec89f8cbb291de6d81f60ccef30735031c7f7b00
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-01 23:28:35 +0300

    added class Packer

---
+++ week9-week9_18.Moving/src/moving/logic/Packer.java
@@ -0,0 +1,21 @@
+package moving.logic;
+
+import moving.domain.Box;
+import moving.domain.Thing;
+
+import java.util.List;
+
+/**
+ * Created by anastasia on 01/01/2017.
+ */
+public class Packer {
+    private int boxesVolume;
+
+    public Packer(int boxesVolume) {
+        this.boxesVolume = boxesVolume;
+    }
+
+    public List<Box> packThings(List<Thing> things) {
+
+    }
+}
